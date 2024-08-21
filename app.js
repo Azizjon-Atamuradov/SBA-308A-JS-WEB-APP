7251afc323f99b4ef17e2aa6f57a401f17939e7d
Aziz <azizatamuradov1994@gmail.com>
2024-08-21 17:04:35 -0400

    date

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 
 const date = document.getElementById('date');
 const city = document.getElementById('city');
-const temp = documnet.getElementById('temp');
+const temp = document.getElementById('temp');
 const tempImg = document.getElementById('tempImg');
 const description = document.getElementById('description');
 const tempMax = document.getElementById('tempMax');
-const tempMin = documnet.getElementById('tempMin');
+const tempMin = document.getElementById('tempMin');
 
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
 let dateObj = new Date();
-let month = months[date.getUTCMonth()]; //// method returns month (from 0 to 11)
+let month = months[dateObj.getUTCMonth()]; //// method returns month (from 0 to 11)
+let day = dateObj.getUTCDate() ; //// returns the day of the month (from 1-31)
+let year = dateObj.getUTCFullYear();
+
+
+date.innerHTML = `${month} ${day}, ${year}`;
+
+
+
 
